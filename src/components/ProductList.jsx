26f9579e208168d76ec2f9699b0e0131c8eb8898
kunnchann <kunnchann9@gmail.com>
2024-08-23 17:21:29 +0700

    ui done

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,72 +1,89 @@
-import { Grid, SimpleGrid, Stack, Tooltip } from "@mantine/core";
+import React from "react";
+import {
+  Button,
+  Group,
+  Loader,
+  SimpleGrid,
+  Stack,
+  Tooltip,
+} from "@mantine/core";
+import useSWR from "swr";
+import { fetcher } from "../util/data";
 
 const ProductList = () => {
-  const data = [
-    {
-      id: 1,
-      title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-      price: 109.95,
-      description:
-        "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-      category: "men's clothing",
-      image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-      rating: { rate: 3.9, count: 120 },
-    },
-    {
-      id: 2,
-      title: "Mens Casual Premium Slim Fit T-Shirts ",
-      price: 22.3,
-      description:
-        "Slim-fitting style, contrast raglan long sleeve, three-button henley placket, light weight & soft fabric for breathable and comfortable wearing. And Solid stitched shirts with round neck made for durability and a great fit for casual fashion wear and diehard baseball fans. The Henley style round neckline includes a three-button placket.",
-      category: "men's clothing",
-      image:
-        "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
-      rating: { rate: 4.1, count: 259 },
-    },
-    {
-      id: 3,
-      title: "Mens Cotton Jacket",
-      price: 55.99,
-      description:
-        "great outerwear jackets for Spring/Autumn/Winter, suitable for many occasions, such as working, hiking, camping, mountain/rock climbing, cycling, traveling or other outdoors. Good gift choice for you or your family member. A warm hearted love to Father, husband or son in this thanksgiving or Christmas Day.",
-      category: "men's clothing",
-      image: "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
-      rating: { rate: 4.7, count: 500 },
-    },
-    {
-      id: 4,
-      title: "Mens Casual Slim Fit",
-      price: 15.99,
-      description:
-        "The color could be slightly different between on the screen and in practice. / Please note that body builds vary by person, therefore, detailed size information should be reviewed below on the product description.",
-      category: "men's clothing",
-      image: "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
-      rating: { rate: 2.1, count: 430 },
-    },
-  ];
+  const { data, isLoading } = useSWR("/api/products", fetcher);
+  const renderStars = (rating) => {
+    const totalStars = 5;
+    const filledStars = Math.round(rating);
+
+    return Array.from({ length: totalStars }, (_, index) => (
+      <svg
+        key={index}
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        stroke={index < filledStars ? "blue" : "currentColor"}
+        className={`w-6 h-6 ${
+          index < filledStars
+            ? "text-blue-500 fill-current"
+            : "text-gray-900 fill-none"
+        }`}
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+        />
+      </svg>
+    ));
+  };
+
   return (
     <SimpleGrid
       cols={{ base: 1, sm: 2, md: 3, lg: 4 }}
-      spacing="xs"
-      verticalSpacing="sm"
+      spacing="xl"
+      verticalSpacing="xl"
+      className={`${isLoading && "flex justify-center items-center h-[500px]"}`}
     >
-      {data.map((product) => (
-        <div className="border p-2 rounded-md border-gray-300" key={product.id}>
-          <Stack gap={"sm"}>
-            <img
-              src={product.image}
-              alt={product.title}
-              className="h-[250px] w-fit block mx-auto"
-            />
-            <Tooltip label={product.title} openDelay={700}>
-              <p className="text-lg font-semibold text-nowrap truncate">
-                {product.title}
+      {isLoading && (
+        <Loader color="blue" size={75} className="mx-auto h-full my-auto" />
+      )}
+      {data &&
+        data.map((product) => (
+          <div
+            className="border p-2 rounded-md border-gray-300"
+            key={product.id}
+          >
+            <Stack gap={"sm"}>
+              <img
+                src={product.image}
+                alt={product.title}
+                className="h-[250px] block mx-auto object-center aspect-auto w-fit"
+              />
+              <Tooltip label={product.title} openDelay={700}>
+                <p className="text-lg font-semibold text-nowrap truncate">
+                  {product.title}
+                </p>
+              </Tooltip>
+              <p className="text-gray-700 text-sm line-clamp-2">
+                {product.description}
               </p>
-            </Tooltip>
-            <p className="text-gray-700 text-sm">{product.description}</p>
-          </Stack>
-        </div>
-      ))}
+              <div className="flex">{renderStars(product.rating.rate)}</div>
+              <Group>
+                <p className="text-lg font-semibold">
+                  ${product.price.toFixed(1)}
+                </p>
+                <Button
+                  className="ms-auto"
+                  onClick={() => addToCart(product)}
+                  variant="outline"
+                  color="blue"
+                >
+                  Add to cart
+                </Button>
+              </Group>
+            </Stack>
+          </div>
+        ))}
     </SimpleGrid>
   );
 };
